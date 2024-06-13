dcfa4bf1de36c2db90e4967b5c7c8da53614b880
KaiqueLimapow <kaiquelima1011@gmail.com>
2024-06-13 12:21:51 -0300

    fizzbuzz

---
+++ app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template('fizzbuzz.html')
+    return app
